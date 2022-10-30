ee009c501585924eb15ede4ead76121e7b7a569d
vicky1312 <victoryabayomi@gmail.com>
2022-10-30 11:07:10 +0100

    updated

--- myapp/src/components/zuri.js
+++ myapp/src/components/zuri.js
@@ -1,9 +1,13 @@
 import React from "react"
 import "./zuri.css"
-import { RiMoreLine } from "react-icons/ri";
-import { RiShareForwardLine } from "react-icons/ri";
-import { BsGithub } from "react-icons/bs";
-import { BsSlack } from "react-icons/bs";
+import "./_Avatar share button.png";
+import "./_Avatar share button mobile.png";
+import {AiOutlineCamera} from "react-icons/ai"
+import "./slack.png"
+import "./Zuri.Internship_Logo.png"
+import "./I4G.png"
+import "./Icon.png";
+
 
 const Greet = () =>{
     return(
@@ -11,20 +15,23 @@ const Greet = () =>{
         <div className="zuriproj">
             <section className="profile">
                 <div id="share">
-                    <RiShareForwardLine />
+                    <img alt="avatar" src={require("./_Avatar share button.png")}></img>
                 </div>
                 <div id="more">
-                    <RiMoreLine />
+                    <img alt="avatar" src={require("./_Avatar share button mobile.png")}></img>
                 </div>
-                <div className="profilepic" id="profile_image">
-                    <img src= {require ("./meme.jpeg")} alt="Victory Abayomi"></img>
+                <div className="profilepic">
+                    <img id="profile_img" src= {require ("./meme.jpeg")} alt="Victory Abayomi"></img>
+                    <div id="camera">
+                        < AiOutlineCamera />
+                    </div>
                 </div>
                 <p id="twitter">endtheworldnowO</p>
                 <p id="slack">Vi</p>
             </section>
             
             <div className="butns">
-                <button id="twitter"> <a href="https://twitter.com/endtheworldnowO">Twitter Link</a></button>
+                <button id="twitters"> <a href="https://twitter.com/endtheworldnowO">Twitter Link</a></button>
                 <button id="butn_zuri"><a href="https://training.zuri.team/" target="_blank" rel="noreferrer">Zuri Team</a></button>
                 <button id="books"><a href=" http://books.zuri.team" target="_blank" rel="noreferrer">Zuri Books</a></button>
                 <button id="book_python"><a href="https://books.zuri.team/python-for-beginners?ref_id=<vi>" target="_blank" rel="noreferrer">Python Books</a></button>
@@ -32,12 +39,26 @@ const Greet = () =>{
                 <button id="book_design"><a href="https://books.zuri.team/design-rules" target="_blank" rel="noreferrer">Design Books</a></button>
             </div>
             <div id="socials">
-                    <a href="https://mail.zuri.team/l/1sw892IMOS9B69KON7AsFDQw/Tf7fWUnd763Xz1DsvpVMzLLQ/YUHM7P7OdX5hgAPlCp0Rsg" 
-                        id="icon" target="_blank" rel="noreferrer"><BsSlack /></a>
+                    <img alt="slack" id="icon" src={require("./slack.png")}></img>
 
-                    <a href="https://github.com/vicky1312" id="icon" target="_blank" rel="noreferrer"><BsGithub /></a>
+                    <img alt="icon" id="icon" src={require("./Icon.png")}></img>
             </div>
         </div>
+
+        <hr></hr>
+        <footer>
+            <div id="foot">
+                <div id="zuri">
+                <img alt="zuri" id="footer_img" src={require("./Zuri.Internship_Logo.png")}></img>
+                </div>
+                <div id="hng">
+                    <p> HNG Internship 9 Frontend Task </p>
+                </div>
+                <div id="i4g">
+                    <img alt="i4g" id="footer_img" src={require("./I4G.png")}></img>
+                </div>
+            </div>
+        </footer>
         </>
     ) 
 } 
